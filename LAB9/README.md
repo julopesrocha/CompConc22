8cf3f0d7a576e09c96c78f1fafe8068a99df7f06
julopesrocha <jujulrocha@gmail.com>
2022-06-29 11:37:02 -0300

    implementação ordem

--- LAB9/README.md
+++ LAB9/README.md
@@ -1,12 +1,11 @@
 # Laboratório 09 | Relatório
-O objetivo deste Laboratorio é introduzir o uso de semáforos.
-
+O objetivo deste Laboratorio é praticar o uso de semáforos para implementar exclusão mútua e sincronização condicional. Vamos repetir o exerício do Lab 5, agora usando semáforos, ao inves de locks e variaveis de condição.
 ## Índice
 * [Informações sobre o hardware](#informações-sobre-o-hardware)
 * [Como rodar o programa](#como-rodar-o-programa)
 * [Análise | Atividade 1 | semaf_1.c](#análise--atividade-1--sem_1.c)
 * [Análise | Atividade 2 | semaf_2.c](#análise--atividade-2--semaf-2c)
-* [Implementação | ](#)
+* [Implementação | LAB9 ](#implementação--lab-9)
 
 ## Informações sobre o hardware
 - Distro: Ubuntu 20.04.4 LTS
@@ -21,6 +20,7 @@ $
 ```
 $ 
 ```
+## Exercícios de Introdução | Semáforos em C
 
 ## Análise | Atividade 1 | semaf_1.c
 
@@ -49,3 +49,24 @@ Não variou. Pois não há condição de corrida entre as threads devido ao valo
 
 - Execute o programa varias vezes e observe os resultados impressos na tela. O que aconteceu e por que? 
 Sim, pois dessa forma o condicionamento de execução das threads não irá mais ser efetivo uma vez que as duas tem liberdade de iniciar, e acessar ao mesmo tempo a zona crítica, causando condição de corrida no sistema.
+
+## Implementação | LAB 9 
+
+**Objetivo**
+Projetar e implementar um programa concorrente onde a ordem de execuc ̧ao ̃
+das threads e controlada no programa.
+
+**Roteiro**
+Implemente um programa com 5 threads:
+- A thread 1 imprime a frase “Volte sempre!”.
+- A thread 2 imprime a frase “Fique a vontade.”
+- A thread 3 imprime a frase “Sente-se por favor.”
+- A thread 4 imprime a frase “Aceita um copo d’agua?.”
+- A thread 5 imprime a frase “Seja bem-vindo!”
+
+As threads devem ser criadas todas de uma vez na func ̧ao main. A ordem de impressão
+das mensagens (execução das threads) deverão ser:
+
+- A thread 5 deve sempre ser a primeira a imprimir sua mensagem.
+- A thread 1 deve sempre ser a ultima a imprimir sua mensagem.
+- A ordem em que as threads 2, 3 e 4 imprimem suas mensagens nao importa, mas todas devem sempre imprimir suas mensagens depois da thread 5 e antes da thread 1.
---
+++ LAB9/ordem.c
@@ -0,0 +1,101 @@
+/* Disciplina: Computacao Concorrente */
+/* Prof.: Silvana Rossetto */
+/* Codigo: Comunicação entre threads usando variável compartilhada e sincronização com semáforos */
+
+#include <stdio.h>
+#include <stdlib.h> 
+#include <pthread.h>
+#include <semaphore.h>
+
+#define NTHREADS 5
+
+// Variaveis globais
+sem_t condt, condt1;     //semaforos para sincronizar a ordem de execucao das threads
+
+//funcao executada pela thread 1
+void *t1 (void *threadid) {
+  int *tid = (int*) threadid;
+  printf("Thread : %d esta executando...\n", *tid);
+  sem_wait(&condt1);
+  printf("Volte sempre!");
+  printf("Thread : %d terminou!\n", *tid);
+  pthread_exit(NULL);
+}
+
+//funcao executada pela thread 2
+void *t2 (void *threadid) {
+  int *tid = (int*) threadid;
+  printf("Thread : %d esta executando...\n", *tid);
+  sem_wait(&condt); //espera T5 executar
+  printf("Fique a vontade.\n");
+  sem_post(&condt);
+  printf("Thread : %d terminou!\n", *tid);
+  sem_post(&condt1);
+  pthread_exit(NULL);
+}
+
+//funcao executada pela thread 3
+void *t3 (void *threadid) {
+    int *tid = (int*) threadid;
+    printf("Thread : %d esta executando...\n", *tid);
+    sem_wait(&condt); //espera T5 executar
+    printf("Sente-se por favor.\n");
+    sem_post(&condt);
+    printf("Thread : %d terminou!\n", *tid);
+    sem_post(&condt1);
+    pthread_exit(NULL);
+}
+
+void *t4 (void *threadid) {
+    int *tid = (int*) threadid;
+    printf("Thread : %d esta executando...\n", *tid);
+    sem_wait(&condt); //espera T5 executar
+    printf("Aceita um copo d'água?\n");
+    sem_post(&condt);
+    printf("Thread : %d terminou!\n", *tid);
+    sem_post(&condt1);
+    pthread_exit(NULL);
+}
+
+void *t5 (void *threadid) {
+    int *tid = (int*) threadid;
+    printf("Thread : %d esta executando...\n", *tid);
+    printf("Seja bem-vindo!\n");
+    sem_post(&condt);
+    printf("Thread : %d terminou!\n", *tid);
+    pthread_exit(NULL);
+}
+
+
+//funcao principal
+int main(int argc, char *argv[]) {
+  pthread_t tid[NTHREADS];
+  int *id[5], t;
+
+  for (t=0; t<NTHREADS; t++) {
+    if ((id[t] = malloc(sizeof(int))) == NULL) {
+       pthread_exit(NULL); return 1;
+    }
+    *id[t] = t+1;
+  }
+
+  //inicia os semaforos
+  sem_init(&condt, 0, 0);
+  sem_init(&condt1, 0, 0);
+
+  //cria as tres threads
+  if (pthread_create(&tid[0], NULL, t1, (void *)id[0])) { printf("--ERRO: pthread_create()\n"); exit(-1); }
+  if (pthread_create(&tid[1], NULL, t2, (void *)id[1])) { printf("--ERRO: pthread_create()\n"); exit(-1); }
+  if (pthread_create(&tid[2], NULL, t3, (void *)id[2])) { printf("--ERRO: pthread_create()\n"); exit(-1); }
+  if (pthread_create(&tid[3], NULL, t4, (void *)id[3])) { printf("--ERRO: pthread_create()\n"); exit(-1); }
+  if (pthread_create(&tid[4], NULL, t5, (void *)id[4])) { printf("--ERRO: pthread_create()\n"); exit(-1); }
+
+  //--espera todas as threads terminarem
+  for (t=0; t<NTHREADS; t++) {
+    if (pthread_join(tid[t], NULL)) {
+         printf("--ERRO: pthread_join() \n"); exit(-1); 
+    } 
+    free(id[t]);
+  } 
+  pthread_exit(NULL);
+}
